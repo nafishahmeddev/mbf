ab697db19e3c815adbfbb873f084db2279c23302
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2022-05-22 12:37:30 +0530

    fetch user location from server

--- lib/_pages/home.page.dart
+++ lib/_pages/home.page.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'dart:math';
-
+import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -186,29 +187,20 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _fetchDonors() async {
-    double zoom = _centerPosition.zoom;
-    double range = ((40000/pow(2, zoom)) * 2)/((150*2));
     Set<Marker> markers = <Marker>{};
-    double random(double mn, double mx) {
-      int fraction = 1000000;
-      int min = (fraction*mn).toInt();
-      int max = (fraction*mx).toInt();
-      double randomValue = (min + Random().nextInt(max - min))/fraction;
-      return randomValue;
-    }
-    for(int x=0; x<random(10.00, 50.00).toInt(); x++){
-      double lat = _centerPosition.target.latitude;
-      double long = _centerPosition.target.longitude;
-      LatLng ltLng= LatLng(
-        random((lat-range), (lat+range)),
-        random((long-range), (long+range)),
-      );
+    //http get request
+    http.Response response = await http.get(Uri.parse("https://nafish.me/map/?zoom=${_centerPosition.zoom}&latitude=${_centerPosition.target.latitude}&longitude=${_centerPosition.target.longitude}"));
+    String body = response.body;
+    List locations = jsonDecode(body);
+    for(List coordinates in locations){
       BitmapDescriptor icon = await BitmapDescriptor.fromAssetImage(const ImageConfiguration(devicePixelRatio: 3.2),"assets/images/markers/${_getRandomMarkerIcon()}.png");
-      markers.add(Marker(
-        markerId: MarkerId('marker_$x'),
-        position: ltLng,
-        icon: icon,
-      ));
+      markers.add(
+          Marker(
+            markerId: MarkerId("marker_${coordinates[0]}_${coordinates[1]}"),
+            position: LatLng(coordinates[0],coordinates[1]),
+            icon: icon,
+          )
+      );
     }
     setState((){
       _markers = markers;
